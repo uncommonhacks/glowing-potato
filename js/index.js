9d1869f9b80b5235e3c8588fbc8a81dbde3865cc
Ben Weinshel <mail@weinshel.net>
2016-07-24 18:30:59 -0500

    refactor cubes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 $( function() {
+  // class draggable is draggable
   $( ".draggable" ).draggable();
-} );
+
+  // class hider button hides parent div
+  $(".hider").click(function(){
+    $(this).parents('div').hide()
+  }); 
+});
+
 function hideDiv(divId) {
    $("#"+divId).hide();
 }
-function startCubeGame() {
-  hideDiv('teaser-box');
-  // CUBE GAME GOES HERE
-}
+// function startCubeGame() {
+//   hideDiv('teaser-box');
+//   // CUBE GAME GOES HERE
+// }
